5f12d4e0602d2be6eb12c5f6222726e7baef0c1d
dyslab <45236395+dyslab@users.noreply.github.com>
2019-10-21 22:56:35 +0800

    Add some test spiders. 🤔

---
+++ spytest/spiders/info_17track.py
@@ -0,0 +1,64 @@
+'''
+    [TEST]
+    
+    Spider name: 17track
+
+    Crawl tracking information from www.17track.net by tracking number.
+
+    Arguments:
+        ! num: Tracking Number.
+
+    Usage:
+        scrapy crawl --nolog 17track -a num=LO091851994CN
+        ! scrapy crawl 17track -a num=LO091851994CN -o 17track.json
+'''
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import JSONRequest
+
+
+class Test17TrackSpider(scrapy.Spider):
+    name = '17track'
+    allowed_domains = ['17track.net']
+    # start_urls = ['https://t.17track.net/restapi/track']
+    # formData: {"data":[{"num":"LO091851994CN","fc":0,"sc":0}],"guid":"","timeZoneOffset":-480}
+    custom_headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'Host': 't.17track.net',
+        'Referer': 'https://t.17track.net/en',
+        'TE': 'Trailers',
+        'Accept': 'application/json',
+        'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3'
+    }
+
+    def start_requests(self):
+        print('>>> post request.')
+        return [JSONRequest(
+            url = "https://t.17track.net/restapi/track",
+            method='POST',
+            headers = self.custom_headers,
+            data = {'data': [{'num': self.num, 'fc': 0, 'sc': 0}], 'guid': '', 'timeZoneOffset': -480},
+            callback = self.after_post
+        )]
+
+    def after_post(self, response):
+        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        #item['name'] = response.xpath('//div[@id="name"]').get()
+        #item['description'] = response.xpath('//div[@id="description"]').get()
+        print('********************** after_post. BEGIN')
+        print(response.url)
+        print(response.headers)
+        print(bytes.decode(response.body))
+        with open('17track.json', 'wb') as fbjson:
+            fbjson.write(b'------------ header part ------------\t\n')
+            for item in dict(response.headers):
+                print('{}: {}'.format(bytes.decode(item), bytes.decode(response.headers[item])))
+                fbjson.write(item)
+                fbjson.write(b': ')
+                fbjson.write(response.headers[item])
+                fbjson.write(b'\t\n')
+
+            fbjson.write(b'\t\n------------- body part -------------\t\n')
+            fbjson.write(response.body)
+            fbjson.close()
+        print('********************** after_post. END')
--- spytest/spiders/info_lptrack.py
+++ spytest/spiders/info_lptrack.py
@@ -1,4 +1,6 @@
 '''
+    [TEST]
+    
     Spider name: lptrack
 
     Crawl tracking information from www.laposte.fr by tracking number.
@@ -7,18 +9,17 @@
         num: Tracking Number.
 
     Usage:
-        scrapy crawl lptrack -a num=LO091851994CN -o t.json
+        scrapy crawl lptrack -a num=LO091851994CN -o lptrack.json
 '''
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import JSONRequest
 
 
-class LPTrackSpider(CrawlSpider):
+class LPTrackSpider(scrapy.Spider):
     name = 'lptrack'
     allowed_domains = ['laposte.fr']
-    # start_urls = ['https://www.laposte.fr/outils/track-a-parcel?code=LO091851994CN']
+    start_urls = ['https://www.laposte.fr/outils/track-a-parcel?code=LO091851994CN']
     # https://api.laposte.fr/ssu/v1/suivi-unifie/idship/LO092265094CN
     custom_headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0',
@@ -29,19 +30,22 @@ class LPTrackSpider(CrawlSpider):
         'Accept-Language': 'zh-CN,en-US;q=0.7,en;q=0.3'
     }
 
-    rules = (
-        Rule(LinkExtractor(allow=r'suivi-unifie/idship/'), callback='parse_item', follow=True),
-    )
-
     def start_requests(self):
         self.custom_headers['Referer'] += self.num
-        yield scrapy.Request(
-            'https://api.laposte.fr/ssu/v1/suivi-unifie/idship/%s' % self.num, 
-            headers=self.custom_headers
+        print('>>> post request.')
+        yield JSONRequest(
+            url = 'https://api.laposte.fr/ssu/v1/suivi-unifie/idship/%s' % self.num,
+            method='GET',
+            headers = self.custom_headers,
+            callback = self.after_get
         )
 
-    def parse_item(self, response):
+    def after_get(self, response):
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
-        return response
+        print('********************** after_post. BEGIN')
+        print(response.url)
+        print(response.headers)
+        print(bytes.decode(response.body))
+        print('********************** after_post. END')
